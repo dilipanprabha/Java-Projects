dda3581c23b3d0ad636cb44ec50d42627448d3ca
Dilipan Prabha <dilipanprabha007@gmail.com>
2024-10-29 14:33:56 +0530

    create some basic functions

--- 6. Bank Account Management System/Bank.java
+++ 6. Bank Account Management System/Bank.java
@@ -1,35 +1,66 @@
+import java.util.HashMap;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 /**
  * Account
  */
 class Account {
-    private String name;
+    public enum AccountType {
+        SAVINGS(1), CHECKING(2), BUSINESS(3);
+
+        private int accountTypeCode;
+
+        private AccountType(int accountTypeCode) {
+            this.accountTypeCode = accountTypeCode;
+        }
+
+        public int getAccountTypeCode() {
+            return accountTypeCode;
+        }
+    }
+
+    private final int SBI_BANK_CODE = 20;
+    private HashMap<String, Integer> branch = new HashMap<>();
+    private String accountHolder;
     private String phone;
-    private int accountNumber;
     private String email;
+    private int accountNumber;
     private double balance;
     private String bankName;
-    private String branch;
     private String IFSC;
     private String accountType;
 
-    public Account(String name, String phone, String email) {
-        this.name = name;
+    public Account(String accountHolder, String phone, String email) {
+        this.accountHolder = accountHolder;
         this.phone = phone;
         this.email = email;
     }
 
+    
+    /** 
+     * Returns the valid string of the user name and email
+     * 
+     * @param scanner object of the Scanner class to get input from user
+     * @param prompt string to display the message to the user
+     * @param match string used to display a error message based on the string(name/email)
+     * @return the valid String
+     */
     public static String getValidString(Scanner scanner, String prompt, String match) {
-        String expression = (match.equals("name")) ? "^[a-zA-Z\\s]+" : "\\b[A-Z0-9._%-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b";
+        final Pattern EMAIL_REGEX = Pattern.compile("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", Pattern.CASE_INSENSITIVE);
+
+        final String NAME_REGEX = "^[a-zA-Z\\s]+";
+
         while (true) {
             System.out.print(prompt);
             try {
                 String name = scanner.nextLine();
-                if (name.toLowerCase().matches(expression) && match.equals("name")) throw new Exception("\n⚠️ Invalid name. Please enter a valid name without numbers or special characters.\n");
+
+                if (!name.toLowerCase().matches(NAME_REGEX) && match.equals("name")) throw new Exception("\n⚠️ Invalid name. Please enter a valid name without numbers or special characters.\n");
                 if (name.length() < 2 && match.equals("name")) throw new Exception("\n⚠️ Name is too short. Please enter a name with at least 2 characters.\n");
                 if (name.length() > 50 && match.equals("name")) throw new Exception("\n⚠️⚠️ Name is too long. Please enter a name with no more than 50 characters.\n");
-                if (name.toLowerCase().matches(expression) && match.equals("email")) throw new Exception("\n⚠️ Invalid email format. Please enter a valid email, e.g., example@domain.com.\n");
+                if (!EMAIL_REGEX.matcher(name).matches() && match.equals("email")) throw new Exception("\n⚠️ Invalid email format. Please enter a valid email, e.g., example@domain.com.\n");
+
                 return name;
             } catch (Exception e) {
                 System.out.println(e.getMessage());
@@ -37,6 +68,14 @@ class Account {
         }
     }
     
+    /** 
+     * Returns the valid string of the user phone
+     * 
+     * @param scanner object of the Scanner class to get input from user
+     * @param prompt string to display the message to the user
+     * @param length used to display a error message based on the size
+     * @return the valid number
+     */
     public static String getValidNumber(Scanner scanner, String prompt, int length) {
         String expression = "[0-9]+";
         while (true) {
@@ -51,6 +90,41 @@ class Account {
             }
         } 
     }
+
+    public int createCustomerNumber() {
+        return (int)Math.floor(Math.random() * 999999) + 100000;
+    }
+
+    public void addBranch(String branchName, int branchCode) {
+        if (checkValidString(branchName)) {
+            System.out.println("Enter correct branch name");
+            return;
+        } 
+        if (checkValidNumber(branchCode, 3)) {
+            System.out.println("Enter correct branch code");
+            return;
+        } 
+
+        branch.put(branchName, branchCode);
+    }
+
+    public static boolean checkValidString(String str) {
+        final String NAME_REGEX = "^[a-zA-Z\\s]+";
+
+        if (str.matches(NAME_REGEX)) return true;
+        return false;
+    }
+
+    public static boolean checkValidNumber(int num, int length) {
+        String strNumber = String.valueOf(num);
+        if (strNumber.length() == length) return true;
+        return false;
+    }
+
+    public int getBranchCode(String branchName) {
+        return branch.get(branchName);
+    }
+
 }
 
 /**
@@ -59,6 +133,5 @@ class Account {
 public class Bank {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
     }
 }
